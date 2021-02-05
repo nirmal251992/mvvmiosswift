f0a593423e4bb7e3793ccd7f512e0397ac800462
nirmal <nirmal251992@gmail.com>
2021-02-05 15:18:36 +0530

    Initial Commit

---
+++ MVVMTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMTest
+//
+//  Created by Nirmal on 05/02/21.
+//  Copyright © 2021 Nirmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
